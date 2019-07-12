973d96c10245971926d09b463e6d2fd52b5862b6
Manki Kim <cpro95@gmail.com>
2019-07-13 00:13:25 +0900

    box struct add

---
+++ box.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"github.com/gdamore/tcell"
+)
+
+// Box implements gui design with a background and optional elements such as a title.
+//
+// See https://github.com/rivo/tview/wiki/Box for original Box implementation
+
+type Box struct {
+	// The position of the rect.
+	x, y, width, height int
+
+	// The inner rect reserved for the box's content.
+	innerX, innerY, innerWidth, innerHeight int
+
+	// Border padding.
+	paddingTop, paddingBottom, paddingLeft, paddingRight int
+
+	// The box's background color.
+	backgroundColor tcell.Color
+
+	// The title. Only visible if there is a border, too.
+	title string
+
+	// The color of the title.
+	titleColor tcell.Color
+
+	// default style
+	defStyle tcell.Style
+}
+
+// NewBox returns a Box without a border.
+func NewBox() *Box {
+	b := &Box{
+		width:           15,
+		height:          10,
+		innerX:          -1, // Mark as uninitialized.
+		backgroundColor: tcell.ColorBlack,
+		titleColor:      tcell.ColorYellow,
+	}
+	return b
+}
+
+// SetBorderPadding sets the size of the borders around the box content.
+func (b *Box) SetBorderPadding(top, bottom, left, right int) *Box {
+	b.paddingTop, b.paddingBottom, b.paddingLeft, b.paddingRight = top, bottom, left, right
+	return b
+}
+
+// GetRect returns the current position of the rectangle, x, y, width, and
+// height.
+func (b *Box) GetRect() (int, int, int, int) {
+	return b.x, b.y, b.width, b.height
+}
+
+// GetInnerRect returns the position of the inner rectangle (x, y, width,
+// height), without the border and without any padding. Width and height values
+// will clamp to 0 and thus never be negative.
+func (b *Box) GetInnerRect() (int, int, int, int) {
+	// default border true
+	border := true
+	if b.innerX >= 0 {
+		return b.innerX, b.innerY, b.innerWidth, b.innerHeight
+	}
+	x, y, width, height := b.GetRect()
+	if border {
+		x++
+		y++
+		width -= 2
+		height -= 2
+	}
+	x, y, width, height = x+b.paddingLeft,
+		y+b.paddingTop,
+		width-b.paddingLeft-b.paddingRight,
+		height-b.paddingTop-b.paddingBottom
+	if width < 0 {
+		width = 0
+	}
+	if height < 0 {
+		height = 0
+	}
+	return x, y, width, height
+}
+
+// SetRect sets a new position of the primitive. Note that this has no effect
+// if this primitive is part of a layout (e.g. Flex, Grid) or if it was added
+// like this:
+//
+//   application.SetRoot(b, true)
+func (b *Box) SetRect(x, y, width, height int) {
+	b.x = x
+	b.y = y
+	b.width = width
+	b.height = height
+	b.innerX = -1 // Mark inner rect as uninitialized.
+}
+
+// SetStyle sets a default style to style
+func (b *Box) SetStyle(style tcell.Style) {
+	b.defStyle = style
+}
+
+// SetBackgroundColor sets the box's background color.
+func (b *Box) SetBackgroundColor(color tcell.Color) *Box {
+	b.backgroundColor = color
+	return b
+}
+
+// SetTitle sets the box's title.
+func (b *Box) SetTitle(title string) *Box {
+	b.title = title
+	return b
+}
+
+// SetTitleColor sets the box's title color.
+func (b *Box) SetTitleColor(color tcell.Color) *Box {
+	b.titleColor = color
+	return b
+}
+
+// Draw draws this primitive onto the screen.
+func (b *Box) Draw(s tcell.Screen) {
+
+	if b.height < b.y {
+		b.y, b.height = b.height, b.y
+	}
+	if b.width < b.x {
+		b.x, b.width = b.width, b.x
+	}
+
+	// Fill background
+	for row := b.y + 1; row < b.height; row++ {
+		for col := b.x + 1; col < b.width; col++ {
+			s.SetContent(col, row, ' ', nil, b.defStyle)
+		}
+	}
+
+	for col := b.x; col <= b.width; col++ {
+		s.SetContent(col, b.y, BoxDrawingsHeavyDoubleDashHorizontal, nil, b.defStyle)
+		s.SetContent(col, b.height, BoxDrawingsHeavyDoubleDashHorizontal, nil, b.defStyle)
+	}
+
+	for row := b.y + 1; row < b.height; row++ {
+		s.SetContent(b.x, row, BoxDrawingsDoubleVertical, nil, b.defStyle)
+		s.SetContent(b.width, row, BoxDrawingsDoubleVertical, nil, b.defStyle)
+	}
+
+	if b.y != b.height && b.x != b.width {
+		// Only add corners if we need to
+		s.SetContent(b.x, b.y, BoxDrawingsDoubleDownAndRight, nil, b.defStyle)
+		s.SetContent(b.width, b.y, BoxDrawingsDoubleDownAndLeft, nil, b.defStyle)
+		s.SetContent(b.x, b.height, BoxDrawingsDoubleUpAndRight, nil, b.defStyle)
+		s.SetContent(b.width, b.height, BoxDrawingsDoubleUpAndLeft, nil, b.defStyle)
+	}
+
+	// Draw title.
+	if b.title != "" && b.width >= 4 {
+		//this is for 2 byte character, insert " " at the end of title and first of line
+		title := " " + b.title + " "
+		emitStr(s, b.x+1, b.y, tcell.StyleDefault.Foreground(b.titleColor), title)
+
+	}
+}
--- main.go
+++ main.go
@@ -1,71 +1,197 @@
+//+build ignore
+//+build ignore
+
+// Copyright 2015 The TCell Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use file except in compliance with the License.
+// You may obtain a copy of the license at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// mouse displays a text box and tests mouse interaction.  As you click
+// and drag, boxes are displayed on screen.  Other events are reported in
+// the box.  Press ESC twice to exit the program.
 package main
 
 import (
 	"fmt"
-	"strconv"
+	"os"
 
 	"github.com/gdamore/tcell"
-	"github.com/rivo/tview"
+	"github.com/gdamore/tcell/encoding"
 )
 
+var defStyle tcell.Style
+
+// func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
+// 	for _, c := range str {
+// 		var comb []rune
+// 		w := runewidth.RuneWidth(c)
+// 		if w == 0 {
+// 			comb = []rune{c}
+// 			c = ' '
+// 			w = 1
+// 		}
+// 		s.SetContent(x, y, c, comb, style)
+// 		x += w
+// 	}
+// }
+
+// params @{r rune} : fill r as white space
+func drawBox(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, r rune) {
+	if y2 < y1 {
+		y1, y2 = y2, y1
+	}
+	if x2 < x1 {
+		x1, x2 = x2, x1
+	}
+
+	for col := x1; col <= x2; col++ {
+		s.SetContent(col, y1, tcell.RuneHLine, nil, style)
+		s.SetContent(col, y2, tcell.RuneHLine, nil, style)
+	}
+	for row := y1 + 1; row < y2; row++ {
+		s.SetContent(x1, row, tcell.RuneVLine, nil, style)
+		s.SetContent(x2, row, tcell.RuneVLine, nil, style)
+	}
+	if y1 != y2 && x1 != x2 {
+		// Only add corners if we need to
+		s.SetContent(x1, y1, tcell.RuneULCorner, nil, style)
+		s.SetContent(x2, y1, tcell.RuneURCorner, nil, style)
+		s.SetContent(x1, y2, tcell.RuneLLCorner, nil, style)
+		s.SetContent(x2, y2, tcell.RuneLRCorner, nil, style)
+	}
+	for row := y1 + 1; row < y2; row++ {
+		for col := x1 + 1; col < x2; col++ {
+			s.SetContent(col, row, r, nil, style)
+		}
+	}
+}
+
+// params @{index int} : index is current index
+func drawList(s tcell.Screen, index int, list []string) {
+	w, _ := s.Size()
+
+	count := len(list)
+
+	white := tcell.StyleDefault.
+		Foreground(tcell.ColorWhite).Background(tcell.ColorBlack)
+	blue := tcell.StyleDefault.Background(tcell.ColorBlue)
+
+	drawBox(s, 1, 20, w-2, 20+count+1, white, ' ')
+	for i := 0; i < count; i++ {
+		if i == index {
+			emitStr(s, 3, 20+1+i, blue, list[i])
+		} else {
+			emitStr(s, 3, 20+1+i, white, list[i])
+		}
+	}
+
+}
+
+// This program just shows simple mouse and keyboard events.  Press ESC twice to
+// exit.
 func main() {
+	encoding.Register()
 
-	f := NewFilesTodo()
-	f.FindData(".")
-
-	app := tview.NewApplication()
-	flex := tview.NewFlex()
-	flex.SetDirection(tview.FlexRow)
-
-	textView := tview.NewTextView()
-	fmt.Fprintf(textView,
-		`Movies Renamer with Subtitles files
-	Tab to Navigate to another panel
-	Y to Rename it
-	Q,CtrlQ,CtrlC to Quit
-		`)
-
-	moviesList := tview.NewList()
-	for i := 0; i < f.m.GetItemCount(); i++ {
-		moviesList.AddItem(f.m.items[i], "", rune(strconv.Itoa(i + 1)[0]), nil)
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
 	}
-	moviesList.SetBorder(true).SetBorderColor(tcell.ColorYellow).SetBorderPadding(1, 1, 1, 1)
-
-	subtitlesList := tview.NewList()
-	for i := 0; i < f.s.GetItemCount(); i++ {
-		subtitlesList.AddItem(f.s.items[i], "", rune(strconv.Itoa(i + 1)[0]), nil)
+	if e := s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
 	}
 
-	subtitlesList.SetBorder(true).SetBorderColor(tcell.ColorBlue).SetBorderPadding(1, 1, 1, 1)
-	flex.AddItem(textView, 0, 1, false)
-	flex.AddItem(moviesList, 0, 4, false)
-	flex.AddItem(subtitlesList, 0, 4, false)
+	// get the w, h of Screen of tcell
+	w, h := s.Size()
 
-	// app.SetAfterDrawFunc(func(screen tcell.Screen) {
-	// 	count := f.m.GetItemCount()
+	// f := NewFilesTodo()
+	// f.FindData(".")
 
-	// 	tview.Print(screen, string(f.m.items[0]), 2, 1, 10, tview.AlignLeft, tcell.ColorYellow)
-	// 	tview.Print(screen, string(f.m.items[1]), 2, 2, 10, tview.AlignLeft, tcell.ColorYellow)
-	// 	tview.Print(screen, strconv.Itoa(count), 2, 30, 10, tview.AlignLeft, tcell.ColorYellow)
-	// })
+	defStyle = tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	s.SetStyle(defStyle)
 
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyCtrlQ {
-			app.Stop()
-		}
-		if event.Key() == tcell.KeyTab {
-			nowPrimitive := app.GetFocus()
-			if nowPrimitive == moviesList {
-				app.SetFocus(subtitlesList)
-			} else if nowPrimitive == subtitlesList {
-				app.SetFocus(moviesList)
+	box := NewBox()
+	box.SetStyle(defStyle)
+	box.SetTitle("Debug Box(디버그 창)")
+	box.SetTitleColor(tcell.ColorYellow)
+
+	// s.EnableMouse()
+	s.Clear()
+
+	keyfmt := "Keys: %s"
+	white := tcell.StyleDefault.
+		Foreground(tcell.ColorWhite).Background(tcell.ColorBlue)
+
+	lks := ""
+	ecnt := 0
+	index := 0
+	for {
+		box.SetRect(1, 1, w-2, 6)
+
+		box.Draw(s)
+		emitStr(s, 3, 2, white, "Press ESC twice or q to exit, C to clear.")
+		emitStr(s, 3, 3, white, fmt.Sprintf("Box Size: (%d,%d) / %d x %d", box.x, box.y, box.width, box.height))
+		x5, y5, x6, y6 := box.GetInnerRect()
+		emitStr(s, 3, 4, defStyle, fmt.Sprintf("Inner Rect Size : (%d,%d) / %d x %d", x5, y5, x6, y6))
+		emitStr(s, 3, 5, white, fmt.Sprintf(keyfmt, lks))
+		// drawList(s, index, f.m.items)
+
+		s.Show()
+		ev := s.PollEvent()
+		st := tcell.StyleDefault.Background(tcell.ColorBlack)
+		w, h = s.Size()
+
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+			// Debug Info to lower right corner, "R" means Resize
+			// s.SetContent(w-1, h-1, 'R', nil, st)
+		case *tcell.EventKey:
+			// ev.Rune() is key what you just pressed
+			// s.SetContent(w-2, h-2, ev.Rune(), nil, st)
+			// s.SetContent(w-1, h-1, 'K', nil, st)
+
+			if ev.Key() == tcell.KeyEscape {
+				ecnt++
+				// escape key was pressed twice it'll quit(exit)
+				if ecnt > 1 {
+					s.Fini()
+					os.Exit(0)
+				}
+			} else if ev.Key() == tcell.KeyCtrlL {
+				s.Sync()
+			} else {
+				ecnt = 0
+				if ev.Rune() == 'C' || ev.Rune() == 'c' {
+					s.Clear()
+				}
+				if ev.Rune() == 'Q' || ev.Rune() == 'q' {
+					s.Fini()
+					os.Exit(0)
+				}
+				if ev.Rune() == 'J' || ev.Rune() == 'j' {
+					index++
+				}
+				if ev.Rune() == 'K' || ev.Rune() == 'k' {
+					index--
+				}
 			}
+			lks = ev.Name()
+		default:
+			s.SetContent(w-1, h-1, 'X', nil, st)
 		}
-		return event
-	})
 
-	if err := app.SetRoot(flex, true).SetFocus(moviesList).Run(); err != nil {
-		panic(err)
 	}
-
 }
